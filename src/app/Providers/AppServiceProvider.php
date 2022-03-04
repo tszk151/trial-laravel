a921c832e812c304238604d55749a5775fac07b0
Tatsuro Suzuki <t.suzuki@salop.co.jp>
2022-03-04 20:28:18 +0900

    [add] Initial setting

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // telescope
+        if ($this->app->isLocal()) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
