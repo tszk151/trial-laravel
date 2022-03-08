5775cf7c8c90c8ed4f486d0b64f7a6bcbb06f492
Tatsuro Suzuki <t.suzuki@salop.co.jp>
2022-03-08 14:26:27 +0900

    [add] install laravel/ui and react

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
